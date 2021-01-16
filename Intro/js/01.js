3d64cde930c1a41bc3096441e48afa319b10176d
Rafael_Estrella <rafaelalfonso82@gmail.com>
2021-01-16 00:26:48 -0400

    In this Episode we saw, Methods, this, v-model and @:click or v-on:click to call a method

--- Intro/js/01.js
+++ Intro/js/01.js
@@ -6,6 +6,15 @@ const app = new Vue({
             {nombre: 'Pera', cantidad: 10},
             {nombre: 'Manzana', cantidad: 0},
             {nombre: 'Platano', cantidad: 11}
-        ]
+        ],
+        nuevaFruta: "",
+    },
+    methods: {
+        agregarFruta(){
+            this.frutas.push({
+                nombre: this.nuevaFruta,
+                cantidad: 0
+            });
+        }
     }
 });
