49a3da8921edbee02c14ca6bda58c18fb01183bc
Rafael_Estrella <rafaelalfonso82@gmail.com>
2021-01-22 01:25:40 -0400

    In this chapter we are using vue components and doing template to use it

---
+++ Components/Assets/components/saludo.js
@@ -0,0 +1,14 @@
+Vue.component('saludo', {
+    template: //html
+    `
+    <div>
+        <h1>{{saludo}}</h1>
+        <h3>kjvkfvs</h3>
+    </div>
+    `,
+    data(){
+        return {
+            saludo: 'Hello World'
+        }
+    }
+})
