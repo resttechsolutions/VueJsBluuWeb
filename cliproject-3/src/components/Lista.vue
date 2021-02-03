b4777cd5b8213ed8de8c378a5c4bbd3914234fc3
Rafael_Estrella <rafaelalfonso82@gmail.com>
2021-02-03 02:07:49 -0400

    A lot of things on Vue

---
+++ cliproject-3/src/components/Lista.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <ul class="list-group">
+      <li
+        v-for="(fruta, index) of arrayOrdenado"
+        :key="fruta.id"
+        @click="aumentar(index)"
+        class="list-group-item d-flex justify-content-between align-items-center"
+      >
+       {{index }} - {{ fruta.nombre }}
+        <span class="badge bg-primary rounded-pill">
+          {{ fruta.cantidad }}
+        </span>
+      </li>
+    </ul>
+    <button class="btn btn-danger btn-block form-control" @click="reiniciar">Reiniciar</button>
+  </div>
+</template>
+<script>
+import { mapState, mapMutations } from "vuex";
+
+export default {
+  name: "Lista",
+  computed: {
+    ...mapState(["frutas"]),
+    arrayOrdenado(){
+        return this.frutas.sort((a,b) => b.cantidad - a.cantidad)
+    }
+  },
+  methods: {
+    ...mapMutations(["aumentar", "reiniciar"]),
+  },
+};
+</script>
---
+++ cliproject2/src/components/Cabecera.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="text-cneter">
+        <img alt="Vue logo" src="@/assets/logo.png">
+         <h1>Frutas</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Cabecera'
+}
+</script>
