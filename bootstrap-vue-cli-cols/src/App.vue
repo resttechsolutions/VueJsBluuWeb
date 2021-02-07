89a7a1ddd89ef2b39c73c30e7c6bd5295536cee5
Rafael_Estrella <rafaelalfonso82@gmail.com>
2021-02-07 06:17:59 -0400

    Working with cols and navbar

---
+++ bootstrap-vue-cli-cols/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <b-navbar toggleable="md" type="dark" variant="success">
+      <b-container>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+        <b-collapse is-nav id="nav-collapse">
+          <b-navbar-nav>
+            <b-nav-item :to="{name: 'Home'}" exact>Home</b-nav-item>
+            <b-nav-item :to="{name: 'About'}" exact>About</b-nav-item>
+            <b-nav-item :to="{name: 'Grid'}">Grid</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
+    </b-navbar>
+
+    <b-container>
+      <router-view />
+    </b-container>
+  </div>
+</template>
+
+
---
+++ bootstrap-vue-cli-cols/src/views/Grid.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="mt-5">
+    <b-row>
+      <b-col cols="10" md="6" lg="4">
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam nihil
+          tempore facilis libero quae tenetur illum obcaecati dicta unde,
+          sapiente facere quam inventore odio saepe, excepturi maiores iusto
+          sint eaque?
+        </p>
+      </b-col>
+      <b-col cols="12" md="6" lg="8">
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam nihil
+          tempore facilis libero quae tenetur illum obcaecati dicta unde,
+          sapiente facere quam inventore odio saepe, excepturi maiores iusto
+          sint eaque?
+        </p>
+      </b-col>
+      <b-col cols="10" md="6" lg="4">
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam nihil
+          tempore facilis libero quae tenetur illum obcaecati dicta unde,
+          sapiente facere quam inventore odio saepe, excepturi maiores iusto
+          sint eaque?
+        </p>
+      </b-col>
+      <b-col cols="12" md="6" lg="8">
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam nihil
+          tempore facilis libero quae tenetur illum obcaecati dicta unde,
+          sapiente facere quam inventore odio saepe, excepturi maiores iusto
+          sint eaque?
+        </p>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+<script>
+export default {
+  name: "Grid",
+};
+</script>
