df5ecb798de6e97de924c89f5e3f20ae714064f6
Rafael_Estrella <rafaelalfonso82@gmail.com>
2021-02-10 01:11:04 -0400

    ok

---
+++ bootstrap-vue-cli/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <b-navbar toggleable="lg" type="dark" variant="success">
+      <b-container>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+        <b-collapse is-nav id="nav-collapse">
+          <b-navbar-nav>
+            <b-nav-item :to="{name: 'Home'}" exact>Home</b-nav-item>
+            <b-nav-item :to="{name: 'About'}">About</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
+    </b-navbar>
+    <router-view/>
+  </div>
+</template>
+
+
--- vue-route-1/src/views/Photos.vue
+++ vue-route-1/src/views/Photos.vue
@@ -1,7 +1,16 @@
 <template>
     <div>
         <h1>Photo Gallery:</h1>
+        <router-link :to="{name: 'Photos', params: {id: pothoArray}}" 
+        v-for="(pothoArray, index) of photosArray" :key="index">
+
+            <button>Photo {{pothoArray}} </button>
+        </router-link>
         <Photography/>
+
+        <button @click="home">Home</button>
+        <button @click="anterior">Anterior</button>
+        <button @click="siguiente">Siguiente</button>
     </div>
 </template>
 
@@ -10,8 +19,25 @@
 import Photography from '@/components/Photography.vue'
 
 export default {
+    name: 'Photos',
     components:{
         Photography
+    },
+    data(){
+        return {
+            photosArray: [1,2,3]
+        }
+    },
+    methods: {
+        home(){
+            this.$router.push('/')
+        }
+    },
+    anterior(){
+        this.$router.go(-1)
+    },
+    siguenete(){
+        this.$router(1)
     }
 }
 </script>
