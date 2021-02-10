9397e40a02d16fda175f7d6ad1082da18ba2f88b
Rafael_Estrella <rafaelalfonso82@gmail.com>
2021-02-10 00:12:49 -0400

    Radio, Select y CheckBox

--- bootstrap-vue-cli-cols/src/views/Forms.vue
+++ bootstrap-vue-cli-cols/src/views/Forms.vue
@@ -4,15 +4,28 @@
 
         <form>
             <b-row>
-                <b-col md="6">
+                <b-col md="3">
                     <b-form-input type="text" v-model="texto" :state="comprobar"></b-form-input>
                     <small>Minimo 3 caracteres</small>
+                    <p>Texto: {{texto}} </p>
+                </b-col>
+                <b-col md="3">
+                    <b-form-select v-model="select" :options="animales"></b-form-select>
+                    <p>Seleccion: {{select}} </p>
+                </b-col>
+                <b-col md="3">
+                    <b-form-radio-group v-model="radio" :options="animalesRadio"></b-form-radio-group>
+                    <p>Radio: {{radio}} </p>
+                </b-col>
+                <b-col md="3">
+                    <b-form-checkbox-group v-model="check" :options="animalesRadio"></b-form-checkbox-group>
+                    <p>Multiple: {{check}} </p>
                 </b-col>
             </b-row>
             
         </form>
 
-        <p>Texto: {{texto}} </p>
+        
     </div>
 </template>
 <script>
@@ -20,7 +33,21 @@ export default {
     name: "Forms",
     data(){
         return {
-            texto: ''
+            texto: '',
+            radio:null,
+            select: null,
+            check: [],
+            animales: [
+                {value: null, text: 'Seleccione un animal'},
+                {value: 'perro', text: 'Guau guau'},
+                {value: 'gato', text: 'Miau'},
+                {value: 'pato', text: 'Cuack'}
+                ],
+            animalesRadio: [
+                {value: 'perro', text: 'Guau guau'},
+                {value: 'gato', text: 'Miau'},
+                {value: 'pato', text: 'Cuack'}
+                ]
         }
     },
     computed:{
