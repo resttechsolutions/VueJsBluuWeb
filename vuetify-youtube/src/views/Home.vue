3a561eac31b370ffd039826b6af5d232d84e5b6f
Rafael_Estrella <rafaelalfonso82@gmail.com>
2021-02-17 02:40:16 -0400

    Working Vuetify with grid, colors, tipography and spacing

--- vuetify-youtube/src/views/Home.vue
+++ vuetify-youtube/src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <v-layout wrap>
+      <v-flex class="primary text-center" xs12>
+        <h1>12</h1>
+      </v-flex>
+      <v-flex class="secondary text-center" xs6>
+        <h1>6</h1>
+      </v-flex>
+      <v-flex class="error text-center" xs6>
+        <h1>6</h1>
+      </v-flex>
+    </v-layout>
+
+    <v-layout secondary mt-5 justify-center align-center style="height:300px">
+      <v-flex class="info text-center" xs3>
+        <h1>Centrado</h1>
+      </v-flex>
+      <v-flex class="pink lighten-1 white--text text-center" xs3>
+        <h1>Centrado</h1>
+      </v-flex>
+    </v-layout>
+
+
+    <v-layout>
+      <v-flex>
+        <h1 class="display-1 text-uppercase">Lorem, ipsum dolor.</h1>
+        <h1 class="display-2 primary pa-5 mt-5">Lorem, ipsum dolor.</h1>
+        <h1 class="display-3 text-center">Lorem, ipsum dolor.</h1>
+        <h1 class="display-4">Lorem, ipsum dolor.</h1>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
