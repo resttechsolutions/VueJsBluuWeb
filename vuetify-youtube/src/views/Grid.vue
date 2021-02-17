49f6a96422892895486e07aeb39b8b7622dc8e90
Rafael_Estrella <rafaelalfonso82@gmail.com>
2021-02-17 00:01:57 -0400

    Working with Vuetify Grid, v-container/v-row/v-cols

---
+++ vuetify-youtube/src/views/Grid.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12" sm="6">
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique
+          maiores odio sequi amet vitae in quam corporis est earum dolores
+          laborum deserunt, voluptate laboriosam vero quia repudiandae quis
+          quidem fugit.
+        </p>
+      </v-col>
+      <v-col cols="12"  sm="6">
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique
+          maiores odio sequi amet vitae in quam corporis est earum dolores
+          laborum deserunt, voluptate laboriosam vero quia repudiandae quis
+          quidem fugit.
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script>
+export default {
+  name: "Grid",
+};
+</script>
+<style>
+</style>
