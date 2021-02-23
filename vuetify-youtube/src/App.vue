d72941a03af9d9647f2fd19bbb13b9afbb4c3ca8
Rafael_Estrella <rafaelalfonso82@gmail.com>
2021-02-22 22:40:08 -0400

    navbar, cards and dialogs

--- vuetify-youtube/src/App.vue
+++ vuetify-youtube/src/App.vue
@@ -1,41 +1,34 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-title>
+        Veutify
+      </v-app-bar-title>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>open_in_new</v-icon>
+      <v-btn class="success">
+        Ingreso
+      </v-btn>
+      <v-btn class="error">
+        Salir
       </v-btn>
     </v-app-bar>
+
+    <v-navigation-drawer app v-model="drawer" temporary dark>
+      
+      <v-layout mt-4 column align-center>
+        <v-flex>
+          <v-avatar>
+            <img src="https://randomuser.me/api/portraits/women/75.jpg" alt="">
+          </v-avatar>
+        </v-flex>
+        <v-flex>
+          <p class="white--text mt-3">Rafael Estrella</p>
+        </v-flex>
+      </v-layout>
+    </v-navigation-drawer>
 
     <v-main>
       <router-view></router-view>
@@ -44,15 +37,15 @@
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
 
-  components: {
+  components: {},
+
+  data() {
+    return {
+      drawer : true
+    }
   },
-
-  data: () => ({
-    //
-  }),
 };
 </script>
--- vuetify-youtube/src/views/Home.vue
+++ vuetify-youtube/src/views/Home.vue
@@ -1,5 +1,81 @@
 <template>
-  <v-container>
+  <v-container grid-list-md>
+    <v-layout row wrap>
+      <v-flex xs12 md6>
+        <v-card>
+          <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg">
+            <v-container>
+              <v-layout>
+                <v-flex>
+                  <span class="headline white--text">Lorem, ipsum dolor.</span>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-img>
+          <v-card-title>
+            <h2>Lorem, ipsum dolor.</h2>
+          </v-card-title>
+          <v-card-text>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet
+              natus vitae nisi exercitationem ducimus minima molestias
+              perspiciatis eius eligendi velit sequi commodi, voluptates
+              doloremque itaque neque odio quam sint facere!
+            </p>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="info" text>Accion</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+      <v-flex xs12 md6>
+        <v-card>
+          <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg">
+            <v-container>
+              <v-layout>
+                <v-flex>
+                  <span class="headline white--text">Lorem, ipsum dolor.</span>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-img>
+          <v-card-title>
+            <h2>Lorem, ipsum dolor.</h2>
+          </v-card-title>
+          <v-card-text>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet
+              natus vitae nisi exercitationem ducimus minima molestias
+              perspiciatis eius eligendi velit sequi commodi, voluptates
+              doloremque itaque neque odio quam sint facere!
+            </p>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="info" text @click="dialog=true">Accion</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+
+    <v-dialog v-model="dialog" width="500" persistent>
+      <v-card>
+        <v-card-title>Este es el Titulo</v-card-title>
+        <v-card-text>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Dignissimos laudantium, dolorem repellat voluptas nisi fugiat ipsum
+            quo odit praesentium sit, id consequatur deleniti dolore deserunt
+            perferendis quasi! Id, quisquam vitae?
+          </p>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn @click="dialog=false">Cerrar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-container>
+  <!-- <v-container>
     <v-layout wrap>
       <v-flex class="primary text-center" xs12>
         <h1>12</h1>
@@ -27,15 +103,18 @@
         <h1 class="display-1 text-uppercase">Lorem, ipsum dolor.</h1>
         <h1 class="display-2 primary pa-5 mt-5">Lorem, ipsum dolor.</h1>
         <h1 class="display-3 text-center">Lorem, ipsum dolor.</h1>
-        <h1 class="display-4 text-right">Lorem, ipsum dolor.</h1>
+        <h1 class="display-4 text-">Lorem, ipsum dolor.</h1>
       </v-flex>
     </v-layout>
-  </v-container>
+  </v-container> -->
 </template>
 
 <script>
-
 export default {
-
-}
+  data() {
+    return {
+      dialog: false,
+    };
+  },
+};
 </script>
