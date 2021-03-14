7c7060c2ecf4163b331f95f1cbc01aa91a7e907f
Rafael_Estrella <rafaelalfonso82@gmail.com>
2021-03-14 14:42:00 -0400

    pushing all worked in the first vue js course finally

---
+++ query-params/src/views/About.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <router-link :to="{query: {seccion: 1}}" class="btn btn-block btn-primary">1</router-link>
+    <router-link :to="{query: {seccion: 2}}" class="btn btn-block btn-primary">2</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+
+    }
+  },
+  watch:{
+    "$route.query.seccion":{
+      inmediate: true,
+      handler(seccion){
+        console.log(`Seccion a cambiado: ${seccion}`);
+      }
+    }
+  },
+}
+</script>
